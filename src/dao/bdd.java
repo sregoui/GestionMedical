618dbbfda90663d4770056b73b0fdc94053c0160
Samia <sregoui@gmail.com>
2014-03-24 23:18:48 +0100

    Ajout classe bdd
    permet la connexion à la base
    Signed-off-by:Samia <sregoui@gmail.com>

--- src/dao/bdd.java
+++ src/dao/bdd.java
@@ -6,10 +6,42 @@
 
 package dao;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Samia
  */
 public class bdd {
+    
+    private Connection cnx;
+    private String username = "";
+    private String password = "";
+    private String url = "jdbc:mysql://";
+
+    public Connection seConnecter() throws ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(bdd.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cnx;
+    }
+    
+    public void seDeconnecter(Connection connection)
+    {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(bdd.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
