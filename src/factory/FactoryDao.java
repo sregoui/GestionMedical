47c57e67bb77ed7dfb108af4d9ba25616f4ae37f
Samia <sregoui@gmail.com>
2014-03-25 11:30:26 +0100

    Creation de la factory
    Pour la reccupération des différents Dao

--- src/factory/FactoryDao.java
+++ src/factory/FactoryDao.java
@@ -6,10 +6,49 @@
 
 package factory;
 
+import contrat.IDao;
+import contrat.Metier;
+
 /**
  *
  * @author Samia
  */
 public class FactoryDao {
+    
+    public static IDao<?> getDAO(Metier objet) {
+
+        String className = "dao.Dao" + objet.getClass().getSimpleName();
+
+        Class<?> cl;
+        Object o = null;
+
+        try {
+            cl = Class.forName(className);
+            o = cl.newInstance();
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+
+        }
+        return (IDao<?>) o;
+    }
+    public static IDao<?> getDAO(String classe) {
+
+       
+        String className = "dao.Dao"+classe;
+
+        Class<?> cl;
+        Object o = null;
+
+        try {
+            cl = Class.forName(className);
+            o = cl.newInstance();
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+
+        }
+        return (IDao<?>) o;
+    }
     
 }
