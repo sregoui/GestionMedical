99a55d2bd7976090342e1c333bc4055b0c7db488
Samia <sregoui@gmail.com>
2014-03-25 11:11:02 +0100

    implementation des methodes dans DaoUtilisateur
    permettre la reccuperation des données

--- src/dao/personnel/DaoUtilisateur.java
+++ src/dao/personnel/DaoUtilisateur.java
@@ -6,12 +6,53 @@
 
 package dao.personnel;
 
-import metier.personnel.*;
+import contrat.IDao;
+import java.sql.ResultSet;
+import java.util.List;
+import metier.personnel.Utilisateur;
+
+
 
 /**
  *
  * @author Samia
  */
-public class DaoUtilisateur {
+public class DaoUtilisateur implements IDao<Utilisateur> {
+
+    @Override
+    public void insert(Utilisateur objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertFromList(List l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Utilisateur objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Utilisateur> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Utilisateur objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ResultSet selectRetunRes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Utilisateur selectById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
