11fc19c86c662f9663478c8a4752d08661fe3828
Samia <sregoui@gmail.com>
2014-03-25 11:04:00 +0100

    Ajout des signatures de methodes à implementer dans l'ensemble des dao
    necessaire à la dao

--- src/contrat/IDao.java
+++ src/contrat/IDao.java
@@ -7,10 +7,22 @@
 
 package contrat;
 
+import dao.bdd;
+import java.sql.ResultSet;
+import java.util.List;
+
 /**
  *
  * @author Samia
  */
-public interface IDao {
-
+public interface IDao<T extends IMetier> {
+    
+    public bdd bdd = new bdd(); 
+    public void insert(T objet);
+    public void insertFromList(List l);
+    public void delete(T objet);
+    public List<T> selectAll();
+    public void update(T objet);
+    public ResultSet selectRetunRes();
+    public T selectById(int id);
 }
