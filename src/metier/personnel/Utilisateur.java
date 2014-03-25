dd8bb1cd5712d4a4cc268ed9bc5f92d92deac91a
Samia <sregoui@gmail.com>
2014-03-25 12:12:53 +0100

    Creation de la classe metier utilisateur avec normes javabeans
    Pour gerer l'authentification

--- src/metier/personnel/Utilisateur.java
+++ src/metier/personnel/Utilisateur.java
@@ -7,11 +7,68 @@
 package metier.personnel;
 
 import contrat.Metier;
+import java.io.Serializable;
 
 /**
  *
  * @author Samia
  */
-public class Utilisateur extends Metier {
+public class Utilisateur extends Metier implements Serializable {
+    
+   private int id;
+   private String login;
+   private String password;
+   private int id_role;
+
+    public Utilisateur(int id, String login, String password, int id_role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.id_role = id_role;
+    }
+
+    public Utilisateur(String login, String password, int id_role) {
+        this.login = login;
+        this.password = password;
+        this.id_role = id_role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId_role() {
+        return id_role;
+    }
+
+    public void setId_role(int id_role) {
+        this.id_role = id_role;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" + "id=" + id + ", login=" + login + ", password=" + password + ", id_role=" + id_role + '}';
+    }
+   
     
 }
