c5171cfe3223f7617c6af3951ec07b661cc2a88f
toiha <issihaca@gmail.com>
2014-03-31 15:44:48 +0200

    class metier Creneau et Rdz modifier

--- src/metier/gestionRdz/Creneau.java
+++ src/metier/gestionRdz/Creneau.java
@@ -14,4 +14,38 @@ import contrat.Metier;
  */
 public class Creneau extends Metier {
     
+    private int id;
+    private String intitule;
+
+    public Creneau(int id, String intitule) {
+        this.id = id;
+        this.intitule = intitule;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public Creneau() {
+    }
+
+    @Override
+    public String toString() {
+        return intitule;
+    }
+    
+    
+    
 }
